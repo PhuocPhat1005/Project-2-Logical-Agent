4ca234db5abf72491659ba39d1611b6532aa284f
Le Phuoc Phat <phatlephuoc1005@gmail.com>
2024-08-06 03:55:15 +0700

    format

---
+++ Source/algorithms/cell.py
@@ -0,0 +1,14 @@
+class Cell:
+    def __init__(self, y, x):
+        self.y = y
+        self.x = x
+        self.value = -1
+        self.element = []
+        self.visited = False
+        self.is_breeze = False
+        self.is_stench = False
+        self.is_whiff = False
+        self.is_glow = False
+        self.is_scream = False
+        self.is_safe = False
+        self.direction = None
