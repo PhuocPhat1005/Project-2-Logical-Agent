a748617f63966f4de3a63dcf150a4417771b111b
Le Phuoc Phat <phatlephuoc1005@gmail.com>
2024-08-12 04:13:40 +0700

    met moi

--- Source/main.py
+++ Source/main.py
@@ -1,14 +1,22 @@
 from algorithms.program import Program
 from algorithms.agent import Agent
 from utils.test import WumpusWorldUI
+from utils.util import Object, Action
+from algorithms.directions import Directions
 
 
 def main():
-    program = Program(file_path="input/map3.txt")
+    program = Program(file_path="input/map1.txt")
     agent = Agent(map_size=program.map_size)
 
-    ui = WumpusWorldUI(program, agent)
-    ui.game_loop()
+    # ui = WumpusWorldUI(program, agent)
+    # ui.game_loop()
+    program.display_map_test()
+    print("hahahaha")
+    action = Action.MOVE_FORWARD
+    agent.perform_action(action, program)
+    print("Point: ", agent.point)
+    print("hihihihi")
 
 
 if __name__ == "__main__":
